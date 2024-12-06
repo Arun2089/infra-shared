63914b49b85d2a9c28fd748bffc7503db0836048
Arun2089 <arunlohar@gkmit.co>
2024-12-06 18:42:21 +0530

    Update ecrpush.groovy

--- vars/ecrpush.groovy
+++ vars/ecrpush.groovy
@@ -1,13 +1,13 @@
 def call(Map config) {
-    def dockerImageName = config.DOCKER_IMAGE_NAME
-    stage('Push to ECR') {
-        steps {
-            withCredentials([string(credentialsId: "${config.SECRET_TOKEN}", variable: "AWS_SECRET_TOKEN")]) {
-                script {
+    def SECRET_TOKEN = config.SECRET_TOKEN ?:error("No token Provided") 
+    def dockerImageName = config.DOCKER_IMAGE_NAME ?:error("No image Provided") 
+    
+            withCredentials([string(credentialsId: "${SECRET_TOKEN}", variable: "setup")]) {
+              
                    
                     def AWS_CREDENTIALS = sh(
                         script: """
-                            set +x; echo ${AWS_SECRET_TOKEN} | awk '{print "-e", \$1, "-e", \$2}'
+                            set +x; echo ${setup} | awk '{print "-e", \$1, "-e", \$2}'
                         """,
                         returnStdout: true
                     ).trim()
@@ -17,8 +17,8 @@ def call(Map config) {
                         echo "Using credentials: ${AWS_CREDENTIALS}"
                         set +x; docker run --rm ${AWS_CREDENTIALS} -v ${workspace}/:/code -v /var/run/docker.sock:/var/run/docker.sock -w /code amazon/aws-cli ecr get-login-password --region ap-south-1 | docker login --username AWS --password-stdin 286291623788.dkr.ecr.ap-south-1.amazonaws.com/dev ;docker push ${dockerImageName}:${BUILD_NUMBER}
                     """
-                }
+                
             }
-        }
-    }
+    
 }
+   
